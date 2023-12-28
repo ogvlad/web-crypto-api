3ca098fcdf958a8e2209e735e3044e4d2a02e463
Vlad Ogay <ogay.vlad@gmail.com>
2023-12-28 11:37:06 +0400

    WebCryptoAES

--- frontend/src/WebCryptoAES/importKey.ts
+++ frontend/src/WebCryptoAES/importKey.ts
@@ -3,5 +3,5 @@ import { fromBase64 } from "../utils/fromBase64"
 
 export async function importKey(aesKeyBase64: Base64String) {
   const aesKeyBytes = fromBase64(aesKeyBase64)
-  return crypto.subtle.importKey("raw", aesKeyBytes, { name: "AES-CBC" }, false, ["encrypt"])
+  return crypto.subtle.importKey("raw", aesKeyBytes, { name: "AES-CBC" }, false, ["encrypt", "decrypt"])
 }
--- frontend/src/WebCryptoAES/index.ts
+++ frontend/src/WebCryptoAES/index.ts
@@ -1,7 +1,6 @@
 import { IAESSecret, IWebCryptoAES } from "../types/IWebCryptoAES"
 import { Base64String } from "../types/Base64String"
 import { generateAESSecret } from "./generateAESSecret"
-import { decryptWithAES } from "./decryptWithAES"
 import { importKey } from "./importKey"
 import { fromBase64 } from "../utils/fromBase64"
 import { toBase64 } from "../utils/toBase64"
@@ -17,9 +16,9 @@ export class WebCryptoAES implements IWebCryptoAES {
       const aesKey = await importKey(secret.key)
       const iv = fromBase64(secret.iv)
       const binary = new TextEncoder().encode(data)
-    
+      
       const encryptedData = await crypto.subtle.encrypt({ name: "AES-CBC", iv }, aesKey, binary)
-    
+      
       return toBase64(encryptedData)
     } catch (error) {
       console.error("Error encrypting with AES:", error)
@@ -28,6 +27,17 @@ export class WebCryptoAES implements IWebCryptoAES {
   }
   
   async decrypt(encryptedData: Base64String, secret: IAESSecret): Promise<string> {
-    return await decryptWithAES(encryptedData, secret.key, secret.iv)
+    try {
+      const aesKey = await importKey(secret.key)
+      const iv = fromBase64(secret.iv)
+      const binary = fromBase64(encryptedData)
+      
+      const decryptedData = await crypto.subtle.decrypt({ name: "AES-CBC", iv }, aesKey, binary)
+      
+      return new TextDecoder().decode(decryptedData)
+    } catch (error) {
+      console.error("Error decrypting with AES:", error)
+      throw error
+    }
   }
 }
