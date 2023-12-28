53fa54c6acff2312d4d472d3db4d8c3a69fccad0
Vlad Ogay <ogay.vlad@gmail.com>
2023-12-28 11:31:02 +0400

    WebCryptoAES

--- frontend/src/WebCryptoAES/index.ts
+++ frontend/src/WebCryptoAES/index.ts
@@ -1,8 +1,10 @@
 import { IAESSecret, IWebCryptoAES } from "../types/IWebCryptoAES"
 import { Base64String } from "../types/Base64String"
 import { generateAESSecret } from "./generateAESSecret"
-import { encryptWithBase64AESKeyAndIV } from "./encryptWithBase64AESKeyAndIV"
 import { decryptWithAES } from "./decryptWithAES"
+import { importKey } from "./importKey"
+import { fromBase64 } from "../utils/fromBase64"
+import { toBase64 } from "../utils/toBase64"
 
 export class WebCryptoAES implements IWebCryptoAES {
   async generate(): Promise<IAESSecret> {
@@ -11,7 +13,18 @@ export class WebCryptoAES implements IWebCryptoAES {
   }
   
   async encrypt(data: string, secret: IAESSecret): Promise<Base64String> {
-    return await encryptWithBase64AESKeyAndIV(data, secret.key, secret.iv)
+    try {
+      const aesKey = await importKey(secret.key)
+      const iv = fromBase64(secret.iv)
+      const binary = new TextEncoder().encode(data)
+    
+      const encryptedData = await crypto.subtle.encrypt({ name: "AES-CBC", iv }, aesKey, binary)
+    
+      return toBase64(encryptedData)
+    } catch (error) {
+      console.error("Error encrypting with AES:", error)
+      throw error
+    }
   }
   
   async decrypt(encryptedData: Base64String, secret: IAESSecret): Promise<string> {
